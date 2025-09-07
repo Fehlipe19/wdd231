0be052d9fe554dc3c7d83bea62b47f0ddedcc970
BrazSAP <codebrazsap@gmail.com>
2025-09-07 18:49:06 -0400

    Add wayfinding in navigation bar

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,3 +5,27 @@ menuButton.addEventListener("click", () => {
   navList.classList.toggle("open");
   menuButton.classList.toggle("active");
 });
+
+const tabs = document.querySelectorAll(".mobile-nav > li > a");
+// console.log(tabs);
+
+// function currentTab (tab) => {
+//   tab.addEventListener("click", () => {
+
+//   });
+// }
+
+function toggleCurrent(tabs) {
+  tabs.forEach((tab) => {
+    if (tab.classList.contains("current-tab")) {
+      tab.classList.remove("current-tab");
+    }
+  });
+}
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    toggleCurrent(tabs);
+    tab.classList.add("current-tab");
+  });
+});
