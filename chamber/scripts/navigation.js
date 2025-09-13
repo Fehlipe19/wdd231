bb25525d53bc144ee3547336777b14dc73366183
BrazSAP <codebrazsap@gmail.com>
2025-09-13 16:23:28 -0400

    Style navigation for mobile view

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,24 @@
+const menuButton = document.querySelector("#ham-menu");
+const navList = document.querySelector("ul");
+
+menuButton.addEventListener("click", () => {
+  navList.classList.toggle("open");
+  menuButton.classList.toggle("active");
+});
+
+const tabs = document.querySelectorAll(".mobile-nav > li > a");
+
+function toggleCurrent(tabs) {
+  tabs.forEach((tab) => {
+    if (tab.classList.contains("current-tab")) {
+      tab.classList.remove("current-tab");
+    }
+  });
+}
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    toggleCurrent(tabs);
+    tab.classList.add("current-tab");
+  });
+});
