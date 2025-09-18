f9042826d28607c081c799de2db6dd95ccf985fa
BrazSAP <codebrazsap@gmail.com>
2025-09-17 23:29:18 -0400

    Display 3-day forecast

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -5,15 +5,26 @@
 const WEATHER_KEY = "a07ecaba27d5b0a33308e8ee9992d95e";
 
 const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=25.8784&lon=-80.1256&appid=${WEATHER_KEY}&units=imperial`;
+const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=25.8784&lon=-80.1256&appid=${WEATHER_KEY}&units=imperial`;
 
 async function fetchWeather() {
   try {
     const response = await fetch(weatherApiUrl);
     const data = await response.json();
     displayCurrentWeather(data);
-    // console.log(data);
   } catch (error) {
     console.error("Error fetching weather data:", error);
+  }
+}
+
+async function fetchForecast() {
+  try {
+    const response = await fetch(forecastApiUrl);
+    const data = await response.json();
+    displayForecast(data);
+    // console.log(data);
+  } catch (error) {
+    console.error("Error fetching forecast data:", error);
   }
 }
 
@@ -27,13 +38,9 @@ function convertToEST(timestamp, isSunrise) {
     epochTime = timestamp.sys.sunset;
     rise = "sunset";
   }
-  //   let timezoneOffSet = timestamp.timezone;
 
   let localTime = epochTime;
   const localDate = new Date(localTime * 1000);
-  console.log(`${rise}: ${localDate.toLocaleTimeString()}`);
-  // console.log(isSunrise);
-  //   console.log(localDate);
 
   return localDate;
 }
@@ -55,7 +62,7 @@ const displayCurrentWeather = (weatherData) => {
   const weatherText = document.createElement("div");
   weatherText.classList.add("weather-text");
   const temperature = document.createElement("p");
-  temperature.innerHTML = `${Math.round(weatherData.main.temp)}&deg;F`;
+  temperature.innerHTML = `<strong>${Math.round(weatherData.main.temp)}&deg;</strong>F`;
   const description = document.createElement("p");
   description.textContent = weatherData.weather[0].description;
   description.style.textTransform = "capitalize";
@@ -74,6 +81,9 @@ const displayCurrentWeather = (weatherData) => {
   const sunsetTime = convertToEST(weatherData, false);
   sunset.textContent = `Sunset: ${sunsetTime.toLocaleTimeString()}`;
 
+  const todayForecast = document.createElement("p");
+  todayForecast.innerHTML = `Today: <strong>${Math.round(weatherData.main.temp)}&deg;</strong>F`;
+  forecastSection.appendChild(todayForecast);
   weatherSection.appendChild(weatherIcon);
   weatherText.appendChild(temperature);
   weatherText.appendChild(description);
@@ -83,9 +93,40 @@ const displayCurrentWeather = (weatherData) => {
   weatherText.appendChild(sunrise);
   weatherText.appendChild(sunset);
   weatherSection.appendChild(weatherText);
+};
 
-  //   console.log(weatherIconURL);
+const displayForecast = (forecastData) => {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const today = new Date();
+  const todayDate = today.getDate();
+  const dayOfWeekNumber = today.getDay();
+  const tomorrowDate = todayDate + 1;
+  const tomorrowDayOfWeekName = days[(dayOfWeekNumber + 1) % 7];
+  const dayAfterTomorrowDate = todayDate + 2;
+  const dayAfterTomorrowDayOfWeekName = days[(dayOfWeekNumber + 2) % 7];
+
+  const forecastList = forecastData.list;
+  forecastList.forEach((forecast) => {
+    const forecastDate = new Date(forecast.dt_txt);
+    const forecastDay = forecastDate.getDate();
+    // console.log(forecastDate.getHours());
+
+    if (forecastDay === tomorrowDate && forecastDate.getHours() === 12) {
+      const forecastTemp = document.createElement("p");
+      forecastTemp.innerHTML = `${tomorrowDayOfWeekName}: <strong>${Math.round(forecast.main.temp)}&deg;</strong>F`;
+      forecastSection.appendChild(forecastTemp);
+      //   console.log(`Tomorrow: ${forecastMain.temp}`);
+    } else if (forecastDay === dayAfterTomorrowDate && forecastDate.getHours() === 12) {
+      const forecastTemp = document.createElement("p");
+      forecastTemp.innerHTML = `${dayAfterTomorrowDayOfWeekName}: <strong>${Math.round(
+        forecast.main.temp
+      )}&deg;</strong>F`;
+      forecastSection.appendChild(forecastTemp);
+      //   console.log(`Day after tomorrow: ${forecastMain.temp}`);
+    }
+  });
 };
 
 // displayCurrentWeather;
 fetchWeather();
+fetchForecast();
