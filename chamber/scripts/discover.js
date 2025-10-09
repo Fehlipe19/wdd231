d40870fd5f5c14d35fa8e3f59ad5e9f7815ee3eb
Fehlipe19 <168611944+Fehlipe19@users.noreply.github.com>
2025-10-09 14:36:34 -0400

    Fix Typo

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,10 +1,38 @@
-const url = "https://fehlipe19.github.io/wdd231/chamber/data/discover.json";
+const attractionURL = "https://fehlipe19.github.io/wdd231/chamber/data/discover.json";
 
 const attraction = document.querySelector("#attractions");
 
 async function getAttractionsData() {
-  const res = await fetch(url);
+  const res = await fetch(attractionURL);
   const data = await res.json();
   console.table(data);
-  // displayAttractions(data.attractions);
+  displayAttractions(data.attractions);
 }
+
+const displayAttractions = (attractions) => {
+  attractions.forEach((attractionInfo) => {
+    const attractionCard = document.createElement("div");
+    const aTitle = document.createElement("h2");
+    const aAddress = document.createElement("p");
+    const aDescription = document.createElement("p");
+    const aFigure = document.createElement("img");
+
+    aTitle.textContent = `${attractionInfo.title}`;
+    aAddress.textContent = `Address: ${attractionInfo.address}`;
+    aDescription.textContent = `${attractionInfo.description}`;
+    aFigure.setAttribute("src", attractionInfo.figure);
+    aFigure.setAttribute("alt", `Image of ${attractionInfo.title}`);
+    aFigure.setAttribute("loading", "lazy");
+    aFigure.setAttribute("width", "300");
+    aFigure.setAttribute("height", "200");
+
+    attractionCard.appendChild(aTitle);
+    attractionCard.appendChild(aAddress);
+    attractionCard.appendChild(aDescription);
+    attractionCard.appendChild(aFigure);
+
+    attraction.appendChild(attractionCard);
+  });
+};
+
+getAttractionsData();
