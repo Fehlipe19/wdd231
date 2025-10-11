7edb936f77fd9714710e81044545274a8f12018c
Fehlipe19 <168611944+Fehlipe19@users.noreply.github.com>
2025-10-10 20:04:03 -0400

    Show time between visits

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -9,3 +9,26 @@ console.log(`Current Year: ${year}`);
 
 currentYear.textContent = `${year}`;
 lastModifiedP.textContent = `Last Modification: ${oLastModified.toLocaleString()}`;
+
+const msToDays = 86400000;
+const currentVisit = Date.now();
+// let lastVisit = 0;
+
+// console.log(`Last visit: ${lastVisit}`);
+
+function calculateDate() {
+  let lastVisit = localStorage.getItem("lastVisit");
+  //   console.log(`Last visit: ${lastVisit}
+  //                 Current Visit: ${currentVisit}`);
+  if (lastVisit === null) {
+    console.log("First time visiting.");
+  } else if (currentVisit - lastVisit < msToDays) {
+    console.log("Last visit was less than 24 hours ago.");
+  } else {
+    const daysSinceLastVisit = Math.round((currentVisit - lastVisit) / msToDays);
+    console.log(`It's been ${daysSinceLastVisit} days since your last visit.`);
+  }
+  localStorage.setItem("lastVisit", currentVisit);
+}
+
+calculateDate();
